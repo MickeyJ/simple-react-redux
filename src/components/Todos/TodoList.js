783b7334c12b7944b22b139f0117515b04a4ff65
Mickey <develop.agility@gmail.com>
2016-04-23 11:30:42 -0600

    delete todo  - heroku push

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,29 +1,27 @@
 import React from 'react'
 
-const TodoList = props => {
-  return (
-    <table> 
-      <tbody>
-        <tr>
-          <th>ID</th>
-          <th>Task</th>
-          <th>Completed</th>
-          <td> </td>
+const TodoList = props => (
+  <table> 
+    <tbody>
+      <tr>
+        <th>ID</th>
+        <th>Task</th>
+        <th>Completed</th>
+        <td> </td>
+      </tr>
+      {props.todos.map((x, i) =>(
+        <tr key={i}>
+          <td>{x.id}</td>
+          <td>{x.task}</td>
+          <td>{x.complete.toString()}</td>
+          <td>
+            <button onClick={() => props.handleDelete (x.id)}>
+              Delete
+            </button>
+          </td>
         </tr>
-        {props.todos.map((x, i) =>(
-          <tr key={i}>
-            <td>{x.id}</td>
-            <td>{x.task}</td>
-            <td>{x.complete.toString()}</td>
-            <td>
-              <button onClick={() => props.handleDelete (x.id)}>
-                Delete
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  )
-};
+      ))}
+    </tbody>
+  </table>
+);
 export default TodoList
